4f89ff57c62bf4eab23eb62931d0265226d63d32
arigid <arigid@gmail.com>
2018-07-19 11:01:28 +0530

    Update sensorSim.py

--- sensorSim.py
+++ sensorSim.py
@@ -10,14 +10,14 @@ while i <60:
                 url = "https://iotabusinesslab.servicebus.windows.net/datacollectoroutbox/publishers/<Put the data collector id>/messages"
                 value = random.randint(40,55)
                 
-                payload = "{\r\n  \"bu\": \"default-unit\",\r\n  \"e\": [\r\n    {\r\n    \t\tbv: null,\r\n    \t\tn:\"sensor end point goes here, refer the other branch DevPy101 for unmasked example\",\r\n    \t\tsv:null,\r\n        \tt:%f,\r\n            u:\"default-unit\",\r\n            v:%d\r\n    }\r\n  ]\r\n}\r\n"
+                payload = "{\r\n  \"bu\": \"default-unit\",\r\n  \"e\": [\r\n    {\r\n    \t\tbv: null,\r\n    \t\tn:\"sensor end point goes here\",\r\n    \t\tsv:null,\r\n        \tt:%f,\r\n            u:\"default-unit\",\r\n            v:%d\r\n    }\r\n  ]\r\n}\r\n"
                 headers = {
-                                'Authorization': "SharedAccessSignature sr=https%3a%2f%2fiotabusinesslab.servicebus.windows.net%2fdatacollectoroutbox%2fpublishers%2f12021a58-0be0-4c8e-9d1a-426182537196%2fmessages&sig=BGuZtITrxRXcxoP23jy3nQHZAjWTerqm9tQdFT3nyYs%3d&se=4687587572&skn=SendAccessPolicy",
+                                'Authorization': "SharedAccessSignature sr=https%3a%2f%2fiotabusinesslab.servicebus.windows.net%2fdatacollectoroutbox%2fpublishers%REPLACED%2fmessages&sig=REPLACED&se=4687587572&skn=SendAccessPolicy",
                                 'Accept': "application/json,text/plain,*/*",
                                 'PayloadType': "application/senml+json",
                                 'X-Requested-With': "XMLHttpRequest",
                                 'Cache-Control': "no-cache",
-                                'Postman-Token': "034eb0a3-0f2a-4e02-84f6-11bf4bcf8e7b"
+                                'Postman-Token': "REPLACED"
                                                 }
 
                 response = requests.request("POST", url, data=payload%(ts,value), headers=headers)
